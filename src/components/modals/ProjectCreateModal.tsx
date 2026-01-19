c54064bc7e0f3bf5e5ea766f5673084d32dd856c
박하은 <hani00415@naver.com>
2026-01-19 15:43:09 +0900

    fix: 모든 요소 위에 모달이 위치하도록 z-index 수정 (사이드바 가림 처리)

--- src/components/modals/ProjectCreateModal.tsx
+++ src/components/modals/ProjectCreateModal.tsx
@@ -24,7 +24,7 @@ const ProjectCreateModal: React.FC<ProjectCreateModalProps> = ({
 
   return (
     <div
-      className="fixed bottom-0 left-[220px] right-0 top-0 z-50 flex items-center justify-center bg-black/50"
+      className="fixed bottom-0 left-0 right-0 top-0 z-[9999] flex items-center justify-center bg-black/50"
       onClick={onClose}
     >
       <div
